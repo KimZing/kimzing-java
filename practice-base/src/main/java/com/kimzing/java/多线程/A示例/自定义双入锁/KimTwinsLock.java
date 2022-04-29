6f9ba4bd07bf031794d14e539397310fdf8d8d0f
kimzing <kimzing@163.com>
2022-04-29 17:40:04 +0800

    添加双线程入口示例

---
+++ practice-base/src/main/java/com/kimzing/java/多线程/A示例/自定义双入锁/KimTwinsLock.java
@@ -0,0 +1,69 @@
+package com.kimzing.java.多线程.A示例.自定义双入锁;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * 支持两个线程同时获取锁.
+ *
+ * @author KimZing - kimzing@163.com
+ * @since 2022/4/29 16:48
+ */
+public class KimTwinsLock {
+
+    private final KimSync kimSync = new KimSync(2);
+
+    public void lock() {
+        // 调用的是模板方法
+        kimSync.acquireShared(1);
+    }
+
+    public void unlock() {
+        // 调用的是模板方法
+        kimSync.releaseShared(1);
+    }
+
+
+    class KimSync extends AbstractQueuedSynchronizer {
+
+        public KimSync(int count) {
+            if (count <= 0) {
+                throw new IllegalArgumentException();
+            }
+            setState(2);
+        }
+
+        /**
+         * 返回负值代表失败，返回0代表当前获取成功，但后续的获取已经没有资源了，会失败。返回正数代表成功
+         *
+         * @param args the acquire argument. This value is always the one
+         *            passed to an acquire method, or is the value saved on entry
+         *            to a condition wait.  The value is otherwise uninterpreted
+         *            and can represent anything you like.
+         * @return
+         */
+        @Override
+        protected int tryAcquireShared(int args) {
+            for (;;) {
+                int expect = getState();
+                int update = expect - args;
+                // 第一个判断条件：如果update为负数，说明没有锁可用，直接返回
+                // 第一个不满足时：如果update>=0, 说明仍有资源可获取，此时再去尝试获取锁(获取成功了则返回，获取失败了则自旋循环再次尝试)
+                if (update < 0  || compareAndSetState(expect, update)) {
+                    return update;
+                }
+            }
+        }
+
+        @Override
+        protected boolean tryReleaseShared(int args) {
+            for (;;) {
+                int expect = getState();
+                int update = expect + args;
+                if (compareAndSetState(expect, update)) {
+                    return true;
+                }
+            }
+        }
+    }
+
+}
---
+++ practice-base/src/main/java/com/kimzing/java/多线程/A示例/自定义双入锁/KimTwinsLockTest.java
@@ -0,0 +1,43 @@
+package com.kimzing.java.多线程.A示例.自定义双入锁;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 双入锁测试.
+ *
+ * <p>
+ *     有四个线程去获取锁，同一时刻有且仅有两个线程能获得锁
+ * </p>
+ *
+ * @author KimZing - kimzing@163.com
+ * @since 2022/4/29 17:27
+ */
+public class KimTwinsLockTest {
+
+
+    public static void main(String[] args) {
+        KimTwinsLock lock = new KimTwinsLock();
+        Runnable runner = () -> {
+            lock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + "获得了锁");
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                System.out.println(Thread.currentThread().getName() + "释放了锁");
+                lock.unlock();
+            }
+
+        };
+        Thread thread1 = new Thread(runner, "thread1");
+        Thread thread2 = new Thread(runner, "thread2");
+        Thread thread3 = new Thread(runner, "thread3");
+        Thread thread4 = new Thread(runner, "thread4");
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+    }
+
+}
